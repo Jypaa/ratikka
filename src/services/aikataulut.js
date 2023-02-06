f3ac491d1ba4dff949c9cb2e443a3991de008865
root <jyri.pappinen@gmail.com>
2023-02-06 17:35:06 +0200

    korjattu aikataulu

--- src/services/aikataulut.js
+++ src/services/aikataulut.js
@@ -1,7 +1,13 @@
 import axios from 'axios'
+const date = new Date();
+const days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday",];
+let day = date.getDay() - 1
+
+console.log("mikä päivä välillä 0-6",day)
+console.log("mikä päivä on",days[day])
 const Url = 'https://data.itsfactory.fi/journeys/api/1/stop-monitoring?stops=0829'
-const Url2= 'https://data.itsfactory.fi/journeys/api/1/journeys?stopPointId=0829'
-const Url3= 'https://data.itsfactory.fi/journeys/api/1/journeys?startIndex=50&stopPointId=0829'
+const Url2= `https://data.itsfactory.fi/journeys/api/1/journeys?stopPointId=0829&dayTypes=${days[day]}`
+const Url3= `https://data.itsfactory.fi/journeys/api/1/journeys?startIndex=50&stopPointId=0829&dayTypes=${days[day]}`
 
 const getAll = () => {
     const request = axios.get(Url)
@@ -11,9 +17,12 @@ const getAll = () => {
 
 const getStop = () =>{
     let aika=new Date(new Date().getTime()+120 *60000).toLocaleTimeString('en-GB', { timeZone: 'UTC' })
-    if( aika> "13:00"){
+    console.log("paljon kello on",aika )
+    console.log( aika> "13:00:00")
+
+    if( aika> "13:00:00"){
         const request = axios.get(Url3)
-        console.log("noudettu pysäkit", request)
+        console.log("noudettu pysäkit url13", request)
         return request.then(response =>response.data)
     }
     else{
